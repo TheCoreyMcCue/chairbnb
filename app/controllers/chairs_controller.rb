760c4de72e7eae7f238ccbeb4339f1904bfd000c
Ludmila Spee <ludmilaspee@web.de>
2021-02-22 17:52:58 +0100

    Create a chairs controller and view

---
+++ app/controllers/chairs_controller.rb
@@ -0,0 +1,9 @@
+class ChairsController < ApplicationController
+  def index
+    @chairs = Chair.all
+  end
+
+  def show
+    @chair = Chair.find(params[:id])
+  end
+end
--- app/models/chair.rb
+++ app/models/chair.rb
@@ -2,11 +2,10 @@ class Chair < ApplicationRecord
   validates :name, presence: true
   validates :location, presence: true
   validates :price, presence: true
-  validates :location, presence: true
-  validates :image, presence: true
+  #validates :image, presence: true
 
-  belongs_to :user, dependant: :destroy
+  belongs_to :user, dependent: :destroy
   has_many :reservations
-  has_many :reviews through: :reservations
+  #has_many :reviews through: :reservations
 
 end

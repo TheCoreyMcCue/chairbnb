4d8bc1f8214c4e3795d9352d2cf4662914c98962
Corey McCue <coreysmccue@gmail.com>
2021-02-25 16:49:42 +0100

    complete mapbox implementation

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -10,13 +10,14 @@ const initMapbox = () => {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
-  }
-  const markers = JSON.parse(mapElement.dataset.markers);
-  markers.forEach((marker) => {
-    new mapboxgl.Marker()
+      const markers = JSON.parse(mapElement.dataset.markers);
+      console.log(markers)
+      markers.forEach((marker) => {
+      new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
       .addTo(map);
-  });
+    });
+  }
 };
 
 // const fitMapToMarkers = (map, markers) => {
